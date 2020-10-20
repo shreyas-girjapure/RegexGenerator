7127ee1fb93cae8b570a6715c5c93b088272c9c6
Sanjog Majarkhede <sanjog.majarkhede@perficient.com>
2020-10-20 18:22:26 +0530

    commited dynamic dropdown

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ document.getElementById('operatorSelect').addEventListener('change',handleOption
 document.getElementById('setSelect').addEventListener('change',handleOptionChange);
 document.getElementById('quantifierSelect').addEventListener('change',handleOptionChange);
 
+//document.getElementById('setSelect').addEventListener('load',setValues);
+
+let setOptions=[
+  {value:"[a-zA-Z]", label:"Alphabets Set"},
+  {value:"[0-9]", label:"Numbers Set"},
+  {value:"\w", label:"Character or Number"},
+  {value:".", label:"Anything"},
+  {value:"\s", label:"Space"},
+  {value:"", label:"Custom String"},
+  {value:"UNSTOPPABLE", label:"UNSTOPPABLE"}
+];
+  setOptions.forEach(setValues);
+
+
+function setValues(item){
+  let inputElement=document.createElement('option');
+  inputElement.setAttribute("class","dropdown-item");
+  inputElement.setAttribute("value",item.value);
+  inputElement.innerHTML=item.label;
+  document.getElementById("setSelect").appendChild(inputElement);
+}
+
 function handleOptionChange(event){
   let value = event.target.value;
   document.getElementById('expression').value += value;
