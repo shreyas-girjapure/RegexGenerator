dd0dc14bf8132e5d3b82dd479e4a561d02550edd
shreyas-girjapure <shreyasgirjapure1122@gmail.com>
2020-10-20 11:53:06 +0530

    changed few lines

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function addRegX() {
-    let x = document.getElementById("mySelect").value;    
-    document.getElementById("expression").value += x;
+
+
+document.getElementById('mySelect').addEventListener('change',handleOptionChange);
+
+
+function handleOptionChange(event){
+  let value = event.target.value;
+  document.getElementById('expression').value = value;
+  generateSampleText(value);
+}
+
+function generateSampleText(value){
+  let sampleExpressionElement = document.getElementById('DataString');
+  switch (value) {
+    case '\\d':
+      sampleExpressionElement.innerText = '4';
+      break;
+    case '[a-zA-Z]':
+      sampleExpressionElement.innerText = 'K';    
+    default:
+      break;
   }
+}
