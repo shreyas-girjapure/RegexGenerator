0554f72e25ce00c8eef94c3825d9423c1984120a
shreyas-girjapure <shreyasgirjapure1122@gmail.com>
2020-10-21 17:08:10 +0530

    seperated expressions

---
+++ expressions.js
@@ -0,0 +1,38 @@
+let analyzerExpressions = [
+    {
+      label: "escapeCharacters",
+      expression: /\[|\]|\(|\)|\.|\^|\$|\?|\||\+|\*/,
+      hint: "hasEscapeCharacters",
+    },
+    {
+      label: "smallAlphabets",
+      expression: /[a-z]/,
+      hint: "hasAlphabets",
+    },
+    {
+      label: "capitalAlphabets",
+      expression: /[A-Z]/,
+      hint: "hasCases",
+    },
+    {
+      label: "numbers",
+      expression: /[0-9]/,
+      hint: "hasNumbers",
+    },
+    {
+      label: "word",
+      expression: /[a-zA-Z0-9]/,
+      hint: "useWord",
+    },
+    {
+      label: "spaces",
+      expression: /[\s]/,
+      hint: "hasSpace",
+    },
+    {
+      label: "symbols",
+      expression: /[!@#%&-="`;,~:{}]/,
+      hint: "hasSpecialSymbols",
+    },
+  ];
+  
